b1f4eeab23a80ef69d59a0fe75673ac0210be5c1
NickitaSolovei <43146250+NickitaSolovei@users.noreply.github.com>
2018-10-07 20:12:45 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 module.exports = function longestConsecutiveLength(array) {
   // your solution here
-  var arrNew = [];
-  for (var i = 0; i < array.length; i++){
-    arrNew[array[i]] = true;
-  }
-
-  var maxLong = 0;
-  var conceivableMaxLong = 0;
-  for (var i = 0; i < arrNew.length; i++) {
+  let arrNew = [];
+  array.forEach(item => {
+    arrNew[item] = true;
+  });
+  let maxLength = 0;
+  let conceivableMaxLength = 0;
+  for (let i = 0; i < arrNew.length; i++) {
     if (arrNew[i]) {
-      conceivableMaxLong++;
-      if (conceivableMaxLong > maxLong){
-        maxLong = conceivableMaxLong;
+      conceivableMaxLength++;
+      if (conceivableMaxLength > maxLength){
+        maxLength = conceivableMaxLength;
       }
     }
-    else conceivableMaxLong = 0;
+    else conceivableMaxLength = 0;
   }
-  return maxLong;
+  return maxLength;
 }
